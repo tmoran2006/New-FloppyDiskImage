4482d2a4bf49cf5f1ea696ab737070ff5807601b
tmoran2006 <125475885+tmoran2006@users.noreply.github.com>
2023-02-15 08:27:50 -0600

    Create NewFloppyDriveImage.psm1

---
+++ NewFloppyDriveImage.psm1
@@ -0,0 +1,20 @@
+Function New-FloppyDriveImage {
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory=$True, Position=0)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Path,
+
+    [Parameter(Mandatory=$True, Position=1)]
+    [ValidateNotNullOrEmpty()]
+    [int]$Size
+  )
+
+  # Create an array of bytes with the specified size
+  $floppy = New-Object Byte[] $Size
+
+  # Write the array of bytes to the specified path
+  [System.IO.File]::WriteAllBytes($Path, $floppy)
+}
+
+Export-ModuleMember -Function New-FloppyDriveImage
